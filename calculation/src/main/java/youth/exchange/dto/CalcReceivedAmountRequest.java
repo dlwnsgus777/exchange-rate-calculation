208850e52f249ac4ac60cff7b84b2959565c810e
Ted <dlwnsgus777@naver.com>
2023-05-15 11:31:24 +0900

    feat: validation 체크 로직 추가
    feat: validation 체크 로직 추가

--- calculation/src/main/java/youth/exchange/dto/CalcReceivedAmountRequest.java
+++ calculation/src/main/java/youth/exchange/dto/CalcReceivedAmountRequest.java
@@ -4,9 +4,17 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Getter(value = AccessLevel.PUBLIC)
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class CalcReceivedAmountRequest {
     private String code;
+
+    @Min(value = 1, message = "0보다 작을 수 없습니다.")
+    @Max(value = 10_000, message = "10,000 보다 클 수 없습니다.")
+    @NotNull(message = "수취 금액을 입력해주세요.")
     private Long money;
 }
--- calculation/src/main/java/youth/exchange/presentation/ExchangeController.java
+++ calculation/src/main/java/youth/exchange/presentation/ExchangeController.java
@@ -3,12 +3,15 @@ package youth.exchange.presentation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import youth.exchange.application.ExchangeService;
 import youth.exchange.dto.CalcReceivedAmountRequest;
 import youth.exchange.dto.CalcReceivedAmountResponse;
 import youth.exchange.dto.GetExchangeRatesResponse;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
@@ -17,7 +20,7 @@ public class ExchangeController {
     private final ExchangeService exchangeService;
 
     @PostMapping("/exchange-rates/calculation")
-    public CalcReceivedAmountResponse calcReceivedAmount(CalcReceivedAmountRequest dto) {
+    public CalcReceivedAmountResponse calcReceivedAmount(@RequestBody @Valid CalcReceivedAmountRequest dto) {
         return exchangeService.calcReceivedAmount(dto);
     }
 
